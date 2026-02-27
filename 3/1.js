d5135e62097df1ed3cec84d5760bdd8eac18d702
danissimurr <rudakov.daniil.2004@mail.ru>
2026-02-27 22:13:27 +0300

    3.1

---
+++ 3/1.js
@@ -0,0 +1,26 @@
+do {
+    let input = prompt("Введите натуральные числа через пробел");
+
+    if (input === null) {
+        break;
+    }
+
+    let words = input.split(" ");
+    let nums = [];
+
+    for (let word of words) {
+        let n = Number(word);
+        if (!isNaN(n) && n > 0) {
+            nums.push(n);
+        }
+    }
+
+    nums.sort(function comp(a, b) {return a - b});
+
+    if (nums.length === 0) {
+        alert("Нет натуралов");
+        continue;
+    }
+    alert(nums.join(" "));
+
+} while (true);
